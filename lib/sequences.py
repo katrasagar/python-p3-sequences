6846a2d673e19c7a4892df7dd352362b3efd590a
Your Name <aqatra6@gmail.com>
2024-08-09 10:04:32 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    """Print the first 'length' Fibonacci numbers."""
+    a, b = 0, 1
+    fibonacci_numbers = []
+
+    for _ in range(length):
+        fibonacci_numbers.append(a)
+
+        
+        a, b = b, a + b
+    print(fibonacci_numbers)
+print_fibonacci(0)
+       
+    
+   
+    
+    
+
